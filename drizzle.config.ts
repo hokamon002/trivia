dc04252ad065581c6cc587f9743d56201e325164
hokamon002 <37353282+hokamon002@users.noreply.github.com>
2023-05-29 03:03:54 -0700

    fix secret leak

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,6 +4,5 @@ import "dotenv/config";
 export default {
   schema: "./src/db/schema.ts",
   connectionString:
-    'mysql://vnzlacsskvaqfnzkg3g6:pscale_pw_sNAAd9oeJfdWlae3gUvm99j6QTKlxn2MXCVBLS2D4CJ@aws.connect.psdb.cloud/trivia?ssl={"rejectUnauthorized":true}',
-  // connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL,
 } satisfies Config;
