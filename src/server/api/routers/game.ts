5b8c0862a64792ab2a67e3cf697307b0cfe7ab35
Andy Tran <andytran0x002@gmail.com>
2023-06-02 11:31:58 -0700

    removed hard-coded data

--- src/server/api/routers/game.ts
+++ src/server/api/routers/game.ts
@@ -2,51 +2,9 @@ import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc";
 import { createInsertSchema } from "drizzle-zod";
 import { games, questions } from "@/db/schema";
 import { z } from "zod";
-import { InferModel } from "drizzle-orm";
+
 const gameSchema = createInsertSchema(games);
 const questionSchema = createInsertSchema(questions);
-
-type GameSchema = InferModel<typeof games, "insert">;
-type QuestionSchema = InferModel<typeof questions, "insert">;
-
-const mockGame: GameSchema = {
-  userId: "itsamemario",
-  id: 123,
-  name: "Mock Game",
-};
-
-const mockQuestions: QuestionSchema[] = [
-  {
-    id: 1,
-    question: "Selbang Airport",
-    answer: "Voltsillam",
-    gameId: 123,
-  },
-  {
-    id: 2,
-    question: "Garuahi Airport",
-    answer: "Voltsillam",
-    gameId: 123,
-  },
-  {
-    id: 3,
-    question: "Pensacola Regional Airport",
-    answer: "Viva",
-    gameId: 123,
-  },
-  {
-    id: 4,
-    question: "Des Moines International Airport",
-    answer: "Flowdesk",
-    gameId: 123,
-  },
-  {
-    id: 5,
-    question: "Yengema Airport",
-    answer: "Konklux",
-    gameId: 123,
-  },
-];
 
 const inputSchema = z.object({
   game: gameSchema,
