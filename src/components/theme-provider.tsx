7bd440f22e5ac8460a6d861f42183a53c54f76d9
Andy Tran <andytran0x002@gmail.com>
2023-06-06 09:25:05 -0700

    fixed undefined localstorage theme

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -5,6 +5,10 @@ import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
 
 const ThemeProvider = ({ children, ...props }: ThemeProviderProps) => {
+  if (!localStorage.getItem("theme")) {
+    localStorage.setItem("theme", "dark");
+  }
+
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 };
 
